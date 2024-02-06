e0518a15c1a3a93ad172026d6807b2ba50cf224a
zy445566 <zyct@vip.qq.com>
2024-02-06 11:53:33 +0800

    feat: env to config

--- config/index.ts
+++ config/index.ts
@@ -2,20 +2,20 @@ export default{
     configServer:{
         // your etcd v3 config
         etcd:{
-            endpoints:['127.0.0.1:2379'],
-            username:'root',
-            password:'123456'
+            endpoints:[process.env.VAAS_PLATFORM_ETCD_ENDPOINT || '127.0.0.1:2379'],
+            username:process.env.VAAS_PLATFORM_ETCD_USERNAME || '',
+            password:process.env.VAAS_PLATFORM_ETCD_PASSWORD || ''
         },
     },
     storageServer:{
         // your minio(s3) config
         minio:{
-            endPoint: '127.0.0.1',
-            port: 9000,
+            endPoint: process.env.VAAS_PLATFORM_MINIO_HOST || '127.0.0.1',
+            port: parseInt(process.env.VAAS_PLATFORM_MINIO_PORT) || 9000,
             useSSL:false,
-            accessKey: 'minio-root-user',
-            secretKey: 'minio-root-password',
-            bucket: 'my-bucket'
+            accessKey: process.env.VAAS_PLATFORM_MINIO_ACCESSKEY|| '',
+            secretKey: process.env.VAAS_PLATFORM_MINIO_SECRETKEY || '',
+            bucket: process.env.VAAS_PLATFORM_MINIO_BUCKET || 'vaas-bucket',
         }
     }
     
